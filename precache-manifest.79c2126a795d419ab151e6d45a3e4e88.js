d9a4ca3535406799a1135c7252409051ff351186
ben.rey <ben.rey@MacDad.local>
2020-01-16 13:39:11 -0600

    Updates

---
+++ precache-manifest.79c2126a795d419ab151e6d45a3e4e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c2936a20e80c9139b05921a30383f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f67fab9db9d69d66bcd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8207e60726e64417627f",
+    "url": "/static/js/2.0260c916.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0260c916.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f67fab9db9d69d66bcd",
+    "url": "/static/js/main.506fc5a8.chunk.js"
+  },
+  {
+    "revision": "851564d85e2e22813d5d",
+    "url": "/static/js/runtime-main.f61e8349.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
